240c566959b4fddd9854c983db1db6047dc6ffa5
Kashvi Garg <97344852+kashvigarg@users.noreply.github.com>
2025-05-16 18:49:24 +0000

    Update Response Config

--- trance-api/internal/middleware/middleware.go
+++ trance-api/internal/middleware/middleware.go
@@ -10,9 +10,10 @@ const UserIDKey contextKey = "userID"
 
 func Corsmiddleware(app http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Refresh-Token")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
--- user/internal/middleware/middleware.go
+++ user/internal/middleware/middleware.go
@@ -6,7 +6,8 @@ import (
 
 func Corsmiddleware(app http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
